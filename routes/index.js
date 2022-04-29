fe3264c231c4288bfc4caa9cf0e4b145d18dbce2
nisha09 <nisha1947@gmail.com>
2022-04-30 01:36:38 +0530

    full functionality

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var router = express.Router();
-const groceries = require('../data/groceries');
+const Groceries = require('../data/groceries');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Grocery List',  groceries  });
+  res.render('index', { title: 'Grocery List',  Groceries  });
 });
 
 router.post('/', function(req, res) {
    req.body.done = false;
-   groceries.push(req.body);
+   Groceries.push(req.body);
   res.redirect('/');
 });
 
+router.delete('/groceries/:id', function(req, res) {
+  const idx = Groceries.find((g) => g.item === req.item);
+  Groceries.splice(idx, 1);
+  res.redirect('/');  
+});
 
 module.exports = router;
