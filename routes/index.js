1c38b1dbece581a9ddc431927b392a4024030a35
nisha09 <nisha1947@gmail.com>
2022-04-30 01:22:07 +0530

    add basic functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const groceries = require('../data/groceries');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Grocery List',  groceries  });
 });
+
+router.post('/', function(req, res) {
+   req.body.done = false;
+   groceries.push(req.body);
+  res.redirect('/');
+});
+
 
 module.exports = router;
